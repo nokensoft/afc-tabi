4ca5fc776ee3e9f5b404359c0aa28a401886fe99
stenlyysamberi <samstenly815@gmail.com>
2023-11-30 14:10:12 +0800

    remove avatar to cht buttom

--- assets/js/wa.js
+++ assets/js/wa.js
@@ -21,7 +21,7 @@ $('#example').czmChatSupport({
     /* Popup Settings */
     popup: {
         automaticOpen: false, /* true or false (Open popup automatically when the page is loaded) */
-        outsideClickClosePopup: true, /* true or false (Clicking anywhere on the page will close the popup) */
+        outsideClickClosePopup: false, /* true or false (Clicking anywhere on the page will close the popup) */
         effect: 1, /* Popup opening effect. Number between 1 and 15 */
         header: {
             backgroundColor: "#000", /* Html color code */
@@ -31,6 +31,37 @@ $('#example').czmChatSupport({
 
         /* Representative Settings */
         persons: [
+
+            
+            /* Copy for more representatives [::Start Copy::] */
+            {
+                avatar: {
+                    src: '<img src="assets/img/whatsapp-avatars/samuel-bosawer.png" alt="Samuel Bosawer">', /* Font, Image or SVG */
+                    backgroundColor: "#10c379", /* Html color code */
+                    onlineCircle: true /* Avatar online circle. To remove, (onlineCircle:false) */
+                },
+                text: {
+                    title: "Samuel Bosawer", /* Writing is required */
+                    description: "Team SaCode", /* Custom text or false. To remove, (description:false) */
+                    online: "Sa Online", /* Custom text or false. To remove, (online:false) */
+                    offline: "Sedang Offline" /* Custom text or false. To remove, (offline:false) */
+                },
+                link: {
+                    desktop: "https://web.whatsapp.com/send?phone=+6282198159714&text=Hi, Samuel!", /* Writing is required */
+                    mobile: "https://wa.me/+6282198159714/?text=Hello, Samuel!" /* If it is hidden desktop link will be valid. To remove, (mobile:false) */
+                },
+                onlineDay: {
+                    /* Change the day you are offline like this. (sunday:false) */
+                    sunday: "09:00-20:59",
+                    monday: "09:00-20:59",
+                    tuesday: "09:00-20:59",
+                    wednesday: "09:00-20:59",
+                    thursday: "09:00-20:59",
+                    friday: "09:00-20:59",
+                    saturday: "09:00-20:59"
+                }
+            },
+            /* [::End Copy::] */
 
             /* Copy for more representatives [::Start Copy::] */
             {
@@ -59,44 +90,16 @@ $('#example').czmChatSupport({
                     friday: "09:00-20:59",
                     saturday: "09:00-20:59"
                 }
-            },
+            }
+
             /* [::End Copy::] */
 
-            /* Copy for more representatives [::Start Copy::] */
-            {
-                // avatar: {
-                //     src: '<img src="assets/img/whatsapp-avatars/samuel-bosawer.png" alt="Samuel Bosawer">', /* Font, Image or SVG */
-                //     backgroundColor: "#10c379", /* Html color code */
-                //     onlineCircle: true /* Avatar online circle. To remove, (onlineCircle:false) */
-                // },
-                // text: {
-                //     title: "Samuel Bosawer", /* Writing is required */
-                //     description: "Team SaCode", /* Custom text or false. To remove, (description:false) */
-                //     online: "Sa Online", /* Custom text or false. To remove, (online:false) */
-                //     offline: "Sedang Offline" /* Custom text or false. To remove, (offline:false) */
-                // },
-                // link: {
-                //     desktop: "https://web.whatsapp.com/send?phone=+6282198159714&text=Hi, Samuel!", /* Writing is required */
-                //     mobile: "https://wa.me/+6282198159714/?text=Hello, Samuel!" /* If it is hidden desktop link will be valid. To remove, (mobile:false) */
-                // },
-                // onlineDay: {
-                //     /* Change the day you are offline like this. (sunday:false) */
-                //     sunday: "09:00-20:59",
-                //     monday: "09:00-20:59",
-                //     tuesday: "09:00-20:59",
-                //     wednesday: "09:00-20:59",
-                //     thursday: "09:00-20:59",
-                //     friday: "09:00-20:59",
-                //     saturday: "09:00-20:59"
-                // }
-            }
-            /* [::End Copy::] */
 
         ]
     },
 
     /* Other Settings */
-    sound: true, /* true (default sound), false or custom sound. Custom sound example, (sound:'assets/sound/notification.mp3') */
+    sound: false, /* true (default sound), false or custom sound. Custom sound example, (sound:'assets/sound/notification.mp3') */
     changeBrowserTitle: "New Message!", /* Custom text or false. To remove, (changeBrowserTitle:false) */
     cookie: false, /* It does not show the speech bubble, notification number, pulse effect and automatic open popup again for the specified time. For example, do not show for 1 hour, (cookie:1) or to remove, (cookie:false) */
 });
